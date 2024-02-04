304d4a76c2b9e4dbe40a80bec8e4ca0ef3a7ffad
Lalchand <dhaka916693@gmail.com>
2024-02-04 16:56:32 +0530

    day-1

---
+++ day-01/owl carousel/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        center: true,
+        nav: true,
+        NavText: [
+            "<i class ='fa fa-angle-left'></i>",
+            "<i class ='fa fa-angle-right'></i>"
+        ],
+        responsive: {
+            0: {
+                items: 1,
+                // nav: true
+            },
+            600: {
+                items: 3,
+                // nav: false
+            },
+            1000: {
+                items: 4,
+                // nav: true,
+                // loop: false
+            }
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
