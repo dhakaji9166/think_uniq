91db91d6c1fcc475409c93d759b0d4badc1a6a92
Lalchand <dhaka916693@gmail.com>
2024-02-12 00:06:12 +0530

    acrothemes and forms dtails

---
+++ day-8/forms/script.js
@@ -0,0 +1,18 @@
+function sendEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "dhaka916693@gmail.com",
+        Password: "Dhaka9166@#$",
+        To: 'lalchanddhaka916693@gmail.com',
+        From: document.getElementById("email").value,
+        Subject: "New Contact Form Enquiry",
+        Body: `
+            Name: ${document.getElementById("name").value}
+            <br> Email: ${document.getElementById("email").value}
+            <br> Phone no: ${document.getElementById("phone").value}
+            <br> Message: ${document.getElementById("message").value}
+        `
+    }).then(
+        message => alert("Message Sent Successfully")
+    );
+}
